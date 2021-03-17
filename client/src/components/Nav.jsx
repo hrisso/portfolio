79314281f7b41f12db64099177a18b3564277c93
hrisso <heather.risso@gmail.com>
2021-03-17 17:32:14 -0400

    styling and resume

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React from 'react';
+import resume from '../assets/HeatherRissoResume.pdf';
 
 export default function Nav() {
   return (
     <div className="nav">
       <h1>Heather Risso</h1>
       <div className="nav-right">
-      <a href=".about-me">About Me</a>
-      <a href=".projects">Projects</a>
-      <a href=".about-me">Resume</a>
+      <a href="#about-me">About Me</a>
+      <a href="#projects">Projects</a>
+        <a href={resume} target="_blank" rel="noopener noreferrer">Resume</a>
       </div>
     </div>
   )
