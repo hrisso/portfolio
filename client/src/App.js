8bb6b61a791a1d3f593f6269526fdb9902fa24b2
hrisso <heather.risso@gmail.com>
2021-03-17 11:09:21 -0400

    react app setup

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Nav from './components/Nav'
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <div className="background-image"></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <div className="nav">
+      <h1>Heather Risso</h1>
+      <div className="nav-right">
+        <p>About Me</p>
+        <p>Projects</p>
+        <p>Resume</p>
+      </div>
+    </div>
+  )
+}
